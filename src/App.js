5bed18e7c435cb823218eca938b35adf39f79370
Adonis Puente <adonispuente@Adoniss-Air.attlocal.net>
2020-03-17 19:24:54 -0400

    no stretch

--- src/App.js
+++ src/App.js
@@ -6,14 +6,17 @@ Look at each file to see where you need to pass props or add code
 */
 import React from "react";
 import "./App.css";
+import SearchBar from "./components/SearchBar/SearchBarContainer";
+import PostsPage from "./components/PostsContainer/PostsPage";
+import Comments from "./components/CommentSection/CommentSectionContainer";
 // import the PostsPage and SearchBar and add them to the App
 
-
 const App = () => {
-
   return (
     <div className="App">
-      {/* Add imported components here to render them */}
+      {/* <Comments /> */}
+      <SearchBar />
+      <PostsPage />
     </div>
   );
 };
